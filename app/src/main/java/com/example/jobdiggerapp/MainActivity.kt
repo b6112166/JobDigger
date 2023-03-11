8c021bdd877e2e34c33c7f7c06f6d60f6e41a7c0
b6112166 <959321146@qq.com>
2023-03-11 14:10:32 -0800

    selection added with animation

--- app/src/main/java/com/example/jobdiggerapp/MainActivity.kt
+++ app/src/main/java/com/example/jobdiggerapp/MainActivity.kt
@@ -6,25 +6,42 @@ import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.core.view.allViews
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+
+
         val logoView = findViewById<ImageView>(R.id.logoImg)
+        val roleSelectionLinearLayout = findViewById<LinearLayout>(R.id.roleSelectionLinearLayout)
+
+        
+
         ObjectAnimator.ofFloat(logoView,"alpha",1f).apply {
             duration = 1000
             start()
             addListener(object : AnimatorListenerAdapter(){
                 override fun onAnimationEnd(animation: Animator) {
-                    ObjectAnimator.ofInt(logoView,"translationY",-300).apply {
-                        duration = 1000
-                        start()
-                    }
+                    logoView.animate().translationY(-700f).setDuration(500);
+                    roleSelectionLinearLayout.animate().alpha(1f).setDuration(1500);
                 }
             })
         }
+
+
+        /*
+        ObjectAnimator.ofFloat(logoView,"layout_marginTop",0f).apply {
+            duration = 1000
+            start()
+        }
+        */
+
 
     }
 
